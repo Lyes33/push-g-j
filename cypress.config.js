7a141f695309360de5a465fcefdac177b34502af
lyes <bob.lyes@live.com>
2024-03-07 21:42:39 +0100

    add jenkins file

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+       "reportDir": "cypress/report",
+       "overwrite": true,
+       "html": true,
+       "json": false
+    },
   chromeWebSecurity: false, 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
