f0a2b0111f61df19e59bd90667b76f01b22eac07
lyes <bob.lyes@live.com>
2024-03-07 22:28:08 +0100

    make test ok

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,6 +3,6 @@ describe('Home spec', () => {
       cy.visit('https://www.saucedemo.com/')
       cy.get('[data-test="username"]').should('be.visible')
       cy.get('[data-test="login-button"]').should('be.visible').and('be.enabled')
-      cy.get('.login_credentials_wrapM').should('be.visible')
+      cy.get('.login_credentials_wrap').should('be.visible')
     })
   })
